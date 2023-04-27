286b9e50752786c608ac9221c842166d1be1212e
Uday Tanthetaa <uday@tanthetaa.co.in>
2023-04-27 11:39:47 +0530

    removed auto reading time

--- config/plugins.js
+++ config/plugins.js
@@ -2,17 +2,6 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
-  "reading-time": {
-    enabled: true,
-    config: {
-      contentTypes: {
-        blog: {
-          field: "readingTime",
-          references: "content",
-        },
-      },
-    },
-  },
   slugify: {
     enabled: true,
     config: {
