3dcce9091448c6d46e34a8973b0032898ce1bad0
Uday Tanthetaa <uday@tanthetaa.co.in>
2023-05-04 14:32:06 +0530

    added logo to portfolio

---
+++ src/api/team-category-tag/controllers/team-category-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * team-category-tag controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::team-category-tag.team-category-tag');
---
+++ src/api/team-category-tag/services/team-category-tag.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * team-category-tag service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::team-category-tag.team-category-tag');
